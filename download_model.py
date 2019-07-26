c64d901876f6602d3b39bda10a984c0bcba21229
Caphodel <caphodel@gmail.com>
2019-07-26 15:54:46 +0700

    add images

---
+++ download_model.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+import glob
+import urllib.request
+import tarfile
+MODEL_FILE = MODEL + '.tar.gz'
+DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+DEST_DIR = '/content/models/research/pretrained_model'
+
+if not (os.path.exists(MODEL_FILE)):
+    urllib.request.urlretrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
+
+tar = tarfile.open(MODEL_FILE)
+tar.extractall()
+tar.close()
+
+os.remove(MODEL_FILE)
+if (os.path.exists(DEST_DIR)):
+    shutil.rmtree(DEST_DIR)
+os.rename(MODEL, DEST_DIR)
